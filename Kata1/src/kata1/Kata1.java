872a1bc0613751257440b745b6467d899996b76d
Aashish Hathiramani <Aashish Hathiramani@Aashish>
2021-09-24 18:50:37 +0100

    Segunda versión de Kata1

--- Kata1/src/kata1/Kata1.java
+++ Kata1/src/kata1/Kata1.java
@@ -1,11 +1,16 @@
 package kata1;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Kata1 {
 
     public static void main(String[] args) {
-        Person person = new Person("Aashish", new Date(99, 8, 27));
+        Calendar birthdate;
+        birthdate = GregorianCalendar.getInstance();
+        birthdate.set(2001, 9, 27);
+        
+        Person person = new Person("Aashish", birthdate);
         System.out.println(person.getName() + " tiene " + person.getAge() + " años");
     }
     
